3f3faf8eb7653489b6cae0412cd5c2df80d20c38
HarrisonLorraway <1011422@student.sae.edu.au>
2019-03-29 10:01:02 +1100

    Test scene with current semi-complete video playing system

---
+++ Purple_Haze/Assets/HarryTest/VideoClip.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Harrison
+{
+    public class VideoClip : MonoBehaviour
+    {
+        public int id;
+        public string fileReference;
+        [HideInInspector] public Button myButton;
+        public enum ButtonState { selected, combining, deselected }
+
+        public ButtonState myState = ButtonState.deselected;
+
+        private void Start()
+        {
+            myButton = GetComponent<Button>();
+        }
+
+        public void ToggleButton()
+        {
+            switch (VideoClipManager.VCM.state)
+            {
+                case VideoClipManager.ManagerState.normal:
+
+                    if (myState == ButtonState.deselected)
+                    {
+                        AddToList(ButtonState.selected);   
+                        CheckColour();
+                        VideoClipManager.VCM.FindVideo();
+                    } 
+                    
+                    break;
+                
+                case VideoClipManager.ManagerState.combining:
+
+                    if (myState == ButtonState.deselected)
+                    {
+                        AddToList(ButtonState.combining);   
+                        CheckColour();
+                    }
+
+                    if (myState == ButtonState.combining)
+                    {
+                        RemoveFromList(ButtonState.deselected);
+                        CheckColour();
+                    }
+                    
+                    break;
+            }
+        }
+        
+        public void AddToList(ButtonState b)
+        {
+            VideoClipManager.VCM.clips.Insert(0, this);
+            myState = b;
+        }
+
+        public void RemoveFromList(ButtonState b)
+        {
+            VideoClipManager.VCM.clips.Remove(this);
+            myState = b;
+        }
+        
+        public void CheckColour()
+        {
+            var colours = myButton.colors;
+            
+            switch (myState)
+            {
+                case ButtonState.deselected:
+                    
+                    colours.normalColor = Color.white;
+                    colours.highlightedColor = Color.white;
+                    myButton.colors = colours;
+                    
+                    break;
+                
+                case ButtonState.selected:
+
+                    colours.normalColor = Color.green;
+                    colours.highlightedColor = Color.green;
+                    myButton.colors = colours;
+                    
+                    break;
+                
+                case ButtonState.combining:
+                    
+                    colours.normalColor = Color.yellow;
+                    colours.highlightedColor = Color.yellow;
+                    myButton.colors = colours;
+                    
+                    break;
+            }
+        }
+
+    }
+}
+
---
+++ Purple_Haze/Assets/HarryTest/VideoClipManager.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Harrison
+{
+    public class VideoClipManager : MonoBehaviour
+    {
+
+        public static VideoClipManager VCM;
+
+        public enum ManagerState { combining, normal }
+        public ManagerState state = ManagerState.normal;
+        
+        public List<VideoClip> clips;
+    
+        private void Awake()
+        {
+            VCM = this;
+        }
+
+        public void FindVideo()
+        {
+            switch (state)
+            {
+                case ManagerState.normal:
+
+                    // Play Video
+
+                    string toPlay = clips[0].fileReference;
+                    // Search For File Reference 
+                    RunVideo(toPlay);
+                    
+                    break;
+                
+                case ManagerState.combining:
+
+                    // Check list + play corresponding video if found
+                    List<int> toPlayList = new List<int>();
+
+                    for (int i = 0; i < clips.Count - 1; i++)
+                    {
+                        toPlayList.Add(clips[i].id);
+                    }
+
+                    RunVideo(toPlayList);
+                    
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// To run a single video just input the file reference
+        /// </summary>
+        /// <param name="s"></param>
+        private void RunVideo(string s)
+        {
+            // Find file matching string 
+        }
+        
+        /// <summary>
+        /// To run a combo video, input a sorted list of id's to search for
+        /// </summary>
+        /// <param name="s"></param>
+        private void RunVideo(List<int> i)
+        {
+            // Find file matching combined id list 
+            
+            // search reference file for matching id
+            // if found play and PurgeList()
+            // if not found PurgeList() and throw message to player
+            
+            i.Sort();
+            
+            string toPlayString = "";
+
+            for (int j = 0; j < clips.Count - 1; j++)
+            {
+                toPlayString = toPlayString + i[j] + ",";
+            }
+
+            toPlayString.Remove(toPlayString.Length - 1);
+            
+        }
+        
+        public void PurgeList()
+        {
+            foreach (VideoClip v in clips)
+            {
+                clips[0].myState = VideoClip.ButtonState.deselected;
+                clips[0].CheckColour();
+                clips.Remove(clips[0]);
+            }
+        }
+
+        public void SwapState()
+        {
+            switch (state)
+            {
+                case ManagerState.normal:
+                    state = ManagerState.combining;
+                    // Hide/Show ui
+                    
+                    break;
+                
+                case ManagerState.combining:
+                    state = ManagerState.normal;
+                    // Hide/Show ui
+                    
+                    break;
+            }
+        }
+        
+    }
+}
+
