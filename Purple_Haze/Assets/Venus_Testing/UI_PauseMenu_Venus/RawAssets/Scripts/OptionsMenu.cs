dab51073f825d90dabc9f72ea5e86621bd5a6804
Unknown <inkyinked@gmail.com>
2019-04-03 16:38:31 +1100

    Created OptionsMenu script; options menu disappears alongside PauseMenu when game is unpaused.

---
+++ Purple_Haze/Assets/Venus_Testing/UI_PauseMenu_Venus/RawAssets/Scripts/OptionsMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsMenu : MonoBehaviour
+{
+    private Canvas OwnCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        OwnCanvas = GetComponent<Canvas>();
+        OwnCanvas.enabled = false;
+        FindObjectOfType<PauseMenu>().ToggleOptions += ShowOptions;
+        //FindObjectOfType<PauseMenu>().ToggleOptions +=
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ShowOptions(string ToggleStr)
+    {
+        switch (ToggleStr)
+        {
+            case "On":
+                OwnCanvas.enabled = true;
+                break;
+            case "Off":
+                OwnCanvas.enabled = false;
+                break;
+        }
+    }
+}
