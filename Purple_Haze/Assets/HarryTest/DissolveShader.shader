e9bb15a28ea5f54d1c7f65a8aac4c2d809d36e9f
HarrisonLorraway <1011422@student.sae.edu.au>
2019-04-22 18:26:43 +1000

    Prep for dissolve shader

---
+++ Purple_Haze/Assets/HarryTest/DissolveShader.shader
@@ -0,0 +1,49 @@
+﻿Shader "Custom/Dissolve" {
+
+	Properties {
+
+		_Color ("Color Tint", Color) = (1,1,1,1)
+		_DissolveColor ("Dissolve Color", Color) = (1,1,1,1)
+		_MainTex ("Albedo (RGB)", 2D) = "white" {}
+		_NoiseMap ("Noise Map", 2D) = "white" {}
+		_DissolveThreshold ("Dissolve Threshold", Range(0,1.2)) = 0.5
+		_ColorThreshold ("Color Threshold", Range(0,1)) = 0.0
+
+	}
+	SubShader {
+
+		Tags { "ForceNoShadowCasting" = "True" "RenderType"="Opaque" }
+		LOD 200
+
+		CGPROGRAM
+		
+		#pragma surface surf Lambert
+		#include "UnityCG.cginc"
+
+		sampler2D _MainTex;
+		sampler2D _NoiseMap;
+		float _DissolveThreshold;
+		float _ColorThreshold;
+		fixed4 _Color;
+		fixed4 _DissolveColor;
+
+		struct Input {
+			float2 uv_MainTex;
+		};
+
+		void surf (Input IN, inout SurfaceOutput o) {
+			// Albedo comes from a texture tinted by color
+			fixed4 c = tex2D(_MainTex, IN.uv_MainTex) * _Color;
+			fixed4 n = tex2D(_NoiseMap, IN.uv_MainTex);
+
+			o.Albedo = c.rgb + lerp(0, _DissolveColor, step(n.r - (_ColorThreshold * _DissolveThreshold) + (0.3 - (0.4 * _DissolveThreshold)), _DissolveThreshold));
+			o.Alpha = c.a;
+			o.Emission = c.rgb + (lerp(0, 1.2, step(n.r - (_ColorThreshold * _DissolveThreshold) + (0.1 - (0.4 * _DissolveThreshold)), _DissolveThreshold)) * 0.9);
+
+			clip(lerp(-1, 1, step(_DissolveThreshold, n.r)));
+
+		}
+		ENDCG
+	}
+	FallBack "Diffuse"
+}
---
+++ Purple_Haze/Assets/HarryTest/DissolveTrigger.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DissolveTrigger : MonoBehaviour {
+
+    Renderer[] rend;
+    WaitForSeconds loopWait = new WaitForSeconds(0.03f); // delay between checking for glitches
+    WaitForSeconds duration = new WaitForSeconds(0.01f); // duration of a glitch
+
+    public bool dying = false;
+
+    public MeshRenderer[] mat;
+    public Texture tex;
+
+    private void Awake()
+    {
+        rend = GetComponentsInChildren<Renderer>();
+
+        mat = GetComponentsInChildren<MeshRenderer>();
+    }
+
+    IEnumerator Start()
+    { 
+
+        while (true)
+        {
+            StartCoroutine(Dissolve());
+            yield return loopWait;
+        }
+
+    }
+
+    IEnumerator Dissolve() // turning on/randomizing glitch function
+    {
+
+        if (dying)
+        {
+            foreach (Renderer r in rend)
+            {
+                r.material.SetFloat("_DissolveThreshold", r.material.GetFloat("_DissolveThreshold") + 0.02f);
+            }
+
+            yield return duration;
+        }
+        else
+        {
+            foreach (Renderer r in rend)
+            {
+                if (r.material.GetFloat("_DissolveThreshold") > 0)
+                {
+                    r.material.SetFloat("_DissolveThreshold", r.material.GetFloat("_DissolveThreshold") - 0.02f);
+                }
+            }
+
+            yield return duration;
+        }
+      
+
+    }
+
+
+}
