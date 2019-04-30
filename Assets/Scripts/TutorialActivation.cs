d21808b2acc50513125c4dce3d94be19eafcee91
SAE\rbuszko <rhysipod@gmail.com>
2019-04-30 14:29:13 +1000

    Final Film upload. Almost all of music added.

--- Assets/Scripts/TutorialActivation.cs
+++ Assets/Scripts/TutorialActivation.cs
@@ -1,18 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TutorialActivation : MonoBehaviour
 {
+
+    public List<GameObject> Activate;
+    public bool disable = true;
     // Start is called before the first frame update
     void Start()
     {
-        
+        foreach(GameObject n in Activate)
+        {
+            n.SetActive(false);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "MainCamera")
+        {
+            if (disable)
+            {
+                foreach (GameObject n in Activate)
+                {
+                    if (n.activeSelf == false)
+                    {
+                        n.SetActive(true);
+                    }
+                }
+            }
+            disable = false;
+
+        }
     }
 }
